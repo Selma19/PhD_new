13ffb0eaebd88c33c56adb5443088fc833e2d1e1
Selma19 <selmakouaiche@gmail.com>
2026-07-26 15:32:45 +0200

    minor

--- sync_data.sh
+++ sync_data.sh
@@ -2,8 +2,5 @@
 
 remoteName="login-dbn01.hpc.gwdg.de"
 
-echo $cidbnROOT
-#echo "{$cidbnROOT}yoyo"
-
 # use rsync so that it knows what was already transfered
-#rsync -chavzP --stats u24849@${remoteName}:${cidbnROOT}data/solo/stimulus.db ./
+rsync -chavzP --stats u24849@${remoteName}:${cidbnROOT}data/solo/stimulus.db ./data/solo/stimulus.db
